7d91306a2c35269a720fb07a8e7be3b9afaf97c0
Naufal Khalis Harahap <55613731+1914370115@users.noreply.github.com>
2019-10-25 18:05:07 +0700

    Add files via upload

---
+++ Naufal Bahasa Algoritma dan Program dasar.c
@@ -0,0 +1,29 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+int main()
+{
+    printf("                                 Naufal Khalis HRP\n");
+    printf("                                     NPM : 1914370115\n");
+    printf("                                  PRODI : Sistem Komputer\n");
+    printf("                                FAKULTAS : Sains Dan Teknologi\n");
+    int a=30;
+    int b=10;
+    int c;
+
+    c=a+b;
+    printf("baris 1 - nilai c adalah %d\n",c);
+    c=a-b;
+    printf("baris 2 - nilai c adalah %d\n",c);
+    c=a*b;
+    printf("baris 3 - nilai c adalah %d\n",c);
+    c=a/b;
+    printf("baris 4 - nilai c adalah %d\n",c);
+    c=a%b;
+    printf("baris 5 - nilai c adalah %d\n",c);
+    c=a++;
+    printf("baris 6 - nilai c adalah %d\n",c);
+    c=a--;
+    printf("baris 7 - nilai c adalah %d\n",c);
+    return 0;
+}
